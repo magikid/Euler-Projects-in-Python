ac35378cea0e39336c9527b3f0e687ebfb4eed22
Chris Jones <chris@christopherjones.us>
2009-02-13 21:56:31 -0500

    Got prime generator working. Next challange will be testing primes against the big number.

--- prob_3.php
+++ prob_3.php
@@ -1,15 +1,39 @@
 <?php
 
-$x=1;
-$primes[0]=2;
+$x=1;$primes[0]=2;
 
-for($y=3;$y<=600851475143;$y+2){
-
-	if($y % $primes[$x-1] != 0){
-		$primes[$x] = $val;
-		$x++;
-		print_r($y);
+function primecandidate($var, $a){
+//	echo 'prime candidate start' . '<br />' . '$var=' . $var . '<br />' . '$a=' . $a . '<br />';
+	
+	global $z, $primes;
+	
+	while($z<$a){
+//	echo "while $z<$a <br /> $var % $primes[$z]:" . $var % $primes[$z] . "<br />";
+		if($var % $primes[$z] != 0){
+//			echo 'made it inside loop <br />';
+			$z++;
+			primecandidate($var, $a);
+		}else{
+//			echo 'failed out <br />';
+			return false;
+		}
 	}
+	
+	return true;
+
 }
+
+for($y=3;$y<=1000;$y+=2){
+//	echo '$y=' . $y . "<br />" . '$primes[$x-1]=' . $primes[$x-1] . "<br />" . '$y % $primes[$x-1]=' . $y % $primes	[$x-1] . "<br />";
+	$z = 0;
+	if(primecandidate($y, $x))
+	{		
+		$primes[$x] = $y;
+		$x++;
+		echo "Prime Found: $y" . "<br />";
+	} else {
+//		echo "Didn't find any <br />";
+	}		
+};
 
 ?>
